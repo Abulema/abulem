2d6eedc839fe5b848473f0a717982c485c46a49e
Abulema <95584046+Abulema@users.noreply.github.com>
2021-12-09 23:24:52 +0100

    Add files via upload

---
+++ Ahlem_jirgi.cpp
@@ -0,0 +1,48 @@
+// lema.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to Ahlem child interview\n";
+    string name;
+    string fname;
+    string mname;
+    string school;
+    string coun;
+    string state;
+
+
+    cout << "What is your Name? \n";
+    cin >> name;
+
+    cout << "What is your Father name? \n";
+    cin >> fname;
+
+    cout << "What is your Mother name? \n";
+    cin >> mname;
+
+    cout << "Which school are you? \n";
+    cin >> school;
+
+    cout << "What is the name of your country? \n";
+    cin >> coun;
+
+    cout << "From which State are you? \n";
+    cin >> state;
+
+    cout << "coal thank you for your answer. let me try to said your information \n";
+
+    cout << "Your name is " << name << " and your father name is " << fname << " and your momcy name is"
+        << mname << endl;
+
+    cout << "Yes is correct countinue \n";
+
+    cout << "you are from " << coun << " and you state is " << state << " state.and your school is " << school << endl;
+    cout << "wow is correct";
+    cout << "ok clap me ";
+}
+
